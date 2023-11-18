b16e643bde5fb748a3a05c5e4bde990d80c940a6
panil <wpeutyf@gmail.com>
2023-11-18 14:37:05 +0200

    123

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 import lxml
-user = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
+user = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0"
 header = {"user-Agent": user}
 session = requests.Session()
 
-for j in range(1, 68):
+for j in range(1, 26):
     print(f"Page = {j}")
-url = "https://rozetka.com.ua/mobile-phones/c80003/"
+url = "https://allo.ua/ua/products/mobile/"
 
 
 
@@ -16,11 +16,14 @@ response = session.get(url, headers=header)
 if response.status_code == 200:
     soup = BeautifulSoup(response.text, "lxml")
 
-all_products = soup.find_all('li', class_="catalog-grid__cell catalog-grid__cell_type_slim ng-star-inserted")
+all_products = soup.find_all('div', class_="products-layout__item without-options-1 without-options-3 without-options-4")
 
 for product in all_products:
-    if product.find("div", class_="goods-tile__price--old price--gray ng-star-inserted"):
-        price = product.find('span', class_="goods-tile__price-value")
-        title = product.find('span', class_="goods-tile__title")
-        print(price.text, title.text)
+    if product.find("div", class_="v-pb__old"):
+        price = product.find('span', class_="v-pb__cur discount")
+        title = product.find("span", class_="product-card__title")
+        print(len(all_products))
+
+
+
 
